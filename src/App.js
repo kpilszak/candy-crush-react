38d89256b2d3f7f4205af342c4b290c116924d71
kpilszak <klaudia.pilszak@gmail.com>
2024-01-02 14:27:18 -0500

    Check for row of three

--- src/App.js
+++ src/App.js
@@ -36,6 +36,20 @@ const App = () => {
         }
     }
 
+    const checkForRowOfThree = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfThree.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
     const createBoard = () => {
         const randomColorArrangement = []
         for (let i = 0; i < width * width; i++) {
@@ -53,10 +67,11 @@ const App = () => {
         const timer = setInterval(() => {
             checkForColumnOfFour()
             checkForColumnOfThree()
+            checkForRowOfThree()
             setCurrentColorArrangement([...currentColorArrangement])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkForColumnOfFour, checkForColumnOfThree])
+    }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfThree])
 
     console.log(currentColorArrangement)
 
