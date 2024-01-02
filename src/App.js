ee06b07202d4fbcf086c4395e81b02465650269f
kpilszak <klaudia.pilszak@gmail.com>
2024-01-02 14:28:48 -0500

    Check for row of four

--- src/App.js
+++ src/App.js
@@ -36,6 +36,20 @@ const App = () => {
         }
     }
 
+    const checkForRowOfFour = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3]
+            const decidedColor = currentColorArrangement[i]
+            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
+
+            if (notValid.includes(i)) continue
+
+            if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfFour.forEach(square => currentColorArrangement[square] = '')
+            }
+        }
+    }
+
     const checkForRowOfThree = () => {
         for (let i = 0; i < 64; i++) {
             const rowOfThree = [i, i + 1, i + 2]
@@ -67,11 +81,12 @@ const App = () => {
         const timer = setInterval(() => {
             checkForColumnOfFour()
             checkForColumnOfThree()
+            checkForRowOfFour()
             checkForRowOfThree()
             setCurrentColorArrangement([...currentColorArrangement])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfThree])
+    }, [checkForColumnOfFour, checkForColumnOfThree, checkForRowOfFour, checkForRowOfThree])
 
     console.log(currentColorArrangement)
 
