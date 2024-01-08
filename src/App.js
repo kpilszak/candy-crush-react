735b88f328b9caf9006b7ea164b4dbe8c96d6ce2
kpilszak <klaudia.pilszak@gmail.com>
2024-01-08 17:00:43 -0500

    Add new square of rows when moving below

--- src/App.js
+++ src/App.js
@@ -66,6 +66,14 @@ const App = () => {
 
     const moveIntoSquareBelow = () => {
         for (let i = 0; i < 64 - width; i++) {
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+            const isFirstRow = firstRow.includes(i)
+
+            if (isFirstRow && currentColorArrangement[i] === '') {
+                let randomNumber = Math.floor(Math.random() * candyColors.length)
+                currentColorArrangement[i] = candyColors[randomNumber]
+            }
+
             if (currentColorArrangement[i + width] === '') {
                 currentColorArrangement[i + width] = currentColorArrangement[i]
                 currentColorArrangement[i] = ''
